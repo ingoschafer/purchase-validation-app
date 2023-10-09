915165f0313af14934933c315be46aaf86329178
Ingo Schaefer <ingo.schaefer@adesso.de>
2023-10-09 17:01:27 +0200

    added zip validation (untested!)

--- extensions/cart-checkout-validation/src/run.js
+++ extensions/cart-checkout-validation/src/run.js
@@ -4,6 +4,7 @@
 /**
 * @typedef {import("../generated/api").RunInput} RunInput
 * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
+* @typedef {import("../generated/api").} CartLine
 */
 
 // The configured entrypoint for the 'purchase.validation.run' extension target
@@ -12,20 +13,39 @@
 * @returns {FunctionRunResult}
 */
 export function run(input) {
-  // The error
-  const error = {
-    localizedMessage: "It is not permitted to buy more than 50 instances of a product at once.",
-    target: "cart"
-  };
-
-  // Parse the decimal (serialized as a string) into a float.
-  const lineQuantityGreaterLimit = input.cart.lines.find(line => line.quantity > 50);
   const errors = [];
+  const lineQuantityError = checkLineQuantities(input.cart.lines)
+  if (!!lineQuantityError) {
+    errors.push(lineQuantityError)
+  }
 
-  // Orders with subtotals greater than $1,000 are available only to established customers.
-  if (!!lineQuantityGreaterLimit) {
-    errors.push(error);
+  const zipError = checkZips(input.cart.deliveryGroups);  
+  if (!!zipError) {
+    errors.push(zipError);
   }
 
   return { errors };
 };
+
+
+
+function checkLineQuantities(cartLines) {
+  const error = {
+    localizedMessage: "It is not permitted to buy more than 50 instances of a product at once.",
+    target: "cart"
+  };
+  const lineQuantityGreaterLimit = cartLines.find(line => line.quantity > 50);
+
+  return !!lineQuantityGreaterLimit ? error : null;
+}
+
+function checkZips(groups) {
+  const error = {
+    localizedMessage: "This zip code is not permitted to purchase products in this shop.",
+    target: "cart"
+  };
+  const legalZipCodes = ["07743", "07749", "07751", "07778", "07745", "07740"];
+  const permittedZipCode = groups.find(group => legalZipCodes.includes(group.deliveryAddress.zip));
+
+  return !permittedZipCode ? null : error;
+}
